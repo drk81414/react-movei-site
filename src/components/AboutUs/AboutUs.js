e4d014848ff017ea99e4ce0feb72ed4df6735503
drk81414 <vikrant4514@gmail.com>
2022-03-27 15:54:05 +0530

    Add linkedin link to AboutUs section

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -37,49 +37,56 @@ const AboutUs = () => {
             image={NoImage}
             name='Mani Raj Gupta'
             rollNo='LCS2021037'
-            Link='LCS2021037@iiitl.ac.in'
+            eMail='LCS2021037@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/mani-raj-gupta-11b329224/'
             phoneNo='+91-7004991816'
           />
           <Profile
             image={NoImage}
             name='Samudrala Sharath Chandra'
             rollNo='LCS2021027'
-            Link='LCS2021027@iiitl.ac.in'
+            eMail='LCS2021027@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/sharath-chandra-4b7023229/'
             phoneNo='+91-9160057443'
           />
           <Profile
             image={RaushanImage}
             name='Raushan kumar'
             rollNo='LCS2021032'
-            Link='LCS2021032@iiitl.ac.in'
+            eMail='LCS2021032@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/raushan-kumar-781a9421a'
             phoneNo='+91-9931946374'
           />
           <Profile
             image={AnimeshImage}
             name='Animesh Sahu'
             rollNo='LCS2021035'
-            Link='LCS2021035@iiitl.ac.in'
+            eMail='LCS2021035@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/animesh-sahu-5a327520b/'
             phoneNo='+91-9165482510'
           />
           <Profile
             image={NoImage}
             name='Abhishek Kumar'
             rollNo='LCS2021036'
-            Link='LCS2021036@iiitl.ac.in'
+            eMail='LCS2021036@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/abhishek-kumar-8a99a6228/'
             phoneNo='+91-9335734573'
           />
           <Profile
             image={SatvikImage}
             name='Satvik Dubey'
             rollNo='LCS2021017'
-            Link='LCS2021017@iiitl.ac.in'
+            eMail='LCS2021017@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/satvik-dubey-1499b1233/'
             phoneNo='+91-9950234151'
           />
           <Profile
             image={AdityaImage}
             name='Aditya Yadav'
             rollNo='LCS2021034'
-            Link='LCS2021036@iiitl.ac.in'
+            eMail='LCS2021036@iiitl.ac.in'
+            linkedIn='https://www.linkedin.com/in/aditya-yadav-a510511a2/'
             phoneNo='+91-8527475970'
           />
       </Wrapper>
--- src/components/AboutUs/Profile/index.js
+++ src/components/AboutUs/Profile/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 //styles
 import { Wrapper, Content, Image, vertical_margin } from './Profile.styles';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const Profile = ({ name, rollNo, Link, phoneNo, image }) => (
+const Profile = ({ name, rollNo, eMail, linkedIn, phoneNo, image }) => (
   <Wrapper>
     <Image src={image} alt="Profile Picture"/>
     <Content>{name} ({rollNo})</Content>
     <Content>
-      <a href={"mailto:" + Link} style={vertical_margin}><FontAwesomeIcon icon={faEnvelope} /></a>
+      <a href={"mailto:" + eMail} style={vertical_margin}><FontAwesomeIcon icon={faEnvelope} /></a>
+      <a href={linkedIn} style={vertical_margin}><FontAwesomeIcon icon={faLinkedin} /></a>
       <a href={"tel:" + phoneNo} style={vertical_margin}><FontAwesomeIcon icon={faPhone} /></a>
     </Content>
   </Wrapper>
